e2bef0e1260b232377c75e1d9c0edd5ba542d3cf
carolly <carolly.barbosa@gmail.com>
2022-01-07 22:21:55 -0300

    first commit

---
+++ app/controller/WeatherController.js
@@ -0,0 +1,14 @@
+class WeatherController {
+    constructor(){
+        this._inputCity = $('#inputCity');
+    }
+    getInput(){
+        return this._inputCity.val()
+    }
+    adiciona(event){
+        event.preventDefault();
+
+        let request = new Api(this.getInput())
+        request.getWeather();
+    }
+}
---
+++ app/model/Api.js
@@ -0,0 +1,34 @@
+class Api {
+
+    constructor(city){
+        this._city = city
+    }
+
+    getCity(){
+        return this._city
+    }
+
+    getWeather(){
+        //chama a requisição
+        let cidade = this.getCity()
+        $.ajax({
+            type: "GET",
+            url: `https://community-open-weather-map.p.rapidapi.com/find?q=${cidade}&cnt=1&mode=null&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric`,
+            headers: {
+                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+                "x-rapidapi-key": "8e1919796cmshecfd43aedaa3cd5p1ee97ejsna4fd79737630"
+            },
+            success: function (response) {
+              console.log(response);
+              console.log(response.list[0].name);
+              console.log(response.list[0].main.temp);
+              console.log(response.list[0].weather[0].description);
+            }
+        });
+    }
+}
+
+//deve fazer a conversão de kelvin para celsius
+//console.log(response.list[0].name);
+//console.log(response.list[0].main.temp);
+//console.log(response.list[0].weather[0].description);
